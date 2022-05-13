526691b01a5e4e3e464b52335c869d07865ec61f
cpak125 <chris.pak125@gmail.com>
2022-05-13 19:20:54 -0400

    sort flight departure times in ascending order

--- controllers/flights.js
+++ controllers/flights.js
@@ -13,7 +13,7 @@ function newFlight(req, res) {
   // Format the date for the value attribute of the input
   let departsDate = `${dt.getFullYear()}-${(dt.getMonth() + 1).toString().padStart(2, '0')}`;
   departsDate += `-${dt.getDate().toString().padStart(2, '0')}T${dt.toTimeString().slice(0, 5)}`;
-  res.render('flights/new', {departsDate});
+  res.render('flights/new', { departsDate });
 }
 
 function create(req, res) {
@@ -32,6 +32,7 @@ function create(req, res) {
 function index(req, res) {
   Flight.find({}, function(err, flights) {
     if (err) return res.redirect('/');
-    res.render('flights/index', {flights});
+    flights = flights.sort((a, b) => a.departs - b.departs);
+    res.render('flights/index', { flights });
   });
 }
